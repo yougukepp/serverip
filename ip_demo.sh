1c5c04182ae72aee1e42c84d196c2e8819b56eec
PengPeng <yougukepp@gmail.com>
2014-08-04 11:34:34 +0800

    2014-08-04 11:34:28

--- ip_demo.sh
+++ ip_demo.sh
@@ -5,7 +5,7 @@
 # https://{username}:{password}@github.com
 # git config --global credential.helper store
 
-# 获取当前时间
+# 获取当前时间 用于提供提交日志
 now=$(date '+%Y-%m-%d %H:%M:%S')
 #echo $now
 
@@ -16,5 +16,5 @@ git add .
 git commit -m "$now"
 
 # 提交至github
-#git push
+git push
 
