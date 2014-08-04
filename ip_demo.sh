22d44a464c431ad77ca3f14a0b63f94e440375cd
PengPeng <yougukepp@gmail.com>
2014-08-04 11:33:11 +0800

    2014-08-04 11:32:59

--- ip_demo.sh
+++ ip_demo.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# 避免git push等待手动输入用户名 密码
+# vim .git-credentials
+# https://{username}:{password}@github.com
+# git config --global credential.helper store
+
+# 获取当前时间
+now=$(date '+%Y-%m-%d %H:%M:%S')
+#echo $now
+
+# 获取外网ip
 curl ifconfig.me/ip > ip
 
+git add .
+git commit -m "$now"
+
+# 提交至github
+#git push
 
