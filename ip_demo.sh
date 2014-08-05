6ef0ba7e29e22a25861e4e3370de61f99618f1d7
PengPeng <yougukepp@gmail.com>
2014-08-05 11:50:20 +0800

    2014-08-05 11:50:01

--- ip_demo.sh
+++ ip_demo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # 避免git push等待手动输入用户名 密码
-# vim .git-credentials
-# https://{username}:{password}@github.com
+cp -rf git-credentials /home/pp/.git-credentials
 # git config --global credential.helper store
+# https://{username}:{password}@github.com
 
 # 获取当前时间 用于提供提交日志
 now=$(date '+%Y-%m-%d %H:%M:%S')
