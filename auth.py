b14788ffee85e213ee66e5faa593760446863082
PengPeng <yougukepp@gmail.com>
2014-08-08 15:19:07 +0800

    2014-08-08 15:19:01

--- auth.py
+++ auth.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import socket
+
+gAuthPort = 8001
+gBufSize = 1024
+
 def main():
-    pass
+    host = ''
+    port = gAuthPort 
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bing((host, port)) 
+    
+    s.listen(1)
+    conn, addr = s.accept()
+    print('Connected by', addr)
+
+    while True:
+        data = conn.recv(gBufSize)
+        if not data:
+            break
+        conn.sendall(all)
+        conn.close()
 
 if __name__ == '__main__':
     main()
