abed61c18992592a6ce94608233b822005b108e7
PengPeng <yougukepp@gmail.com>
2014-08-08 15:21:10 +0800

    2014-08-08 15:21:01

--- auth.py
+++ auth.py
@@ -12,7 +12,7 @@ def main():
     
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((host, port)) 
-    
+
     s.listen(1)
     conn, addr = s.accept()
     print('Connected by', addr)
